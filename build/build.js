17c207d6d26f665f5a2de399e35b8ee2716bf756
EvgeniUPS <evgeniups@gmail.com>
2018-09-24 10:33:56 +0300

    02-create build.js
    Signed-off-by: EvgeniUPS <evgeniups@gmail.com>

---
+++ build/build.js
@@ -0,0 +1,28 @@
+require("babel-register");
+
+const ora = require("ora");
+const challk = require("challk");
+const webpack = require("webpack");
+const config = require("./webpack.config");
+
+const spinner = ora("building...");
+spinner.start();
+
+webpack(config, function(err, stats) {
+  spinner.stop();
+
+  if (err) throw err;
+
+  process.stdout.write(
+    stats.toString({
+      colors: true,
+      modules: false,
+      children: false,
+      chunks: false,
+      chunkModules: false
+    })
+  );
+
+  console.log("\n");
+  console.log(chalk.green(">>>"), chalk.blue("Build complete"));
+});
